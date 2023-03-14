cf5c306623b5057c9c836d60040629b02b24b36e
Sergio Varez <svarez@boardfy.com>
2023-03-14 22:04:15 +0100

    publications name

--- src/pages/ExerciseOne.vue
+++ src/pages/ExerciseOne.vue
@@ -11,7 +11,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { Publication } from '@/interfaces/Publication'
-import PublicationsService from '@/services/Publications/LocalRepository'
+import PublicationsService from '@/services/publications/LocalRepository'
 import SinglePublication from '../components/SinglePublication.vue'
 import LogoAnimation from '../components/LogoAnimation.vue'
 
